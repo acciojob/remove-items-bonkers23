408323aa3d6b1d403f026ad8732d2b2c54f79f67
Shariq Hashmi <127607558+bonkers23@users.noreply.github.com>
2025-05-04 16:38:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function removeColor() {
+  const select = document.getElementById("colorSelect");
+  const selectedIndex = select.selectedIndex;
+
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+}
